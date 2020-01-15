f6f807c936b8050ebf04665980f6524077b087ce
Moksleivis <moksleivis@KITM.local>
2020-01-15 13:23:35 +0200

    nuskaitom is failo, surikiuojam

--- main.cpp
+++ main.cpp
@@ -1,31 +1,53 @@
 #include <iostream>
 #include<vector>
+#include<fstream>
+#include<algorithm>
 
 using namespace std;
+
+ifstream in("duomenys.txt");
+ofstream out("res.txt");
+
+void skaitomPusinam(vector<int> &A);
+void spausdink(vector<int> A, string text);
+void rikiavimas(vector<int> &A);
+
 
 int main()
 {
 
-    vector<int> V;
-    V.push_back(2);
-    V.push_back(-17);
-    int a;
-    cout<<"a= ";
-    cin>>a;
-    V.push_back(a);
-    V.insert(V.begin(),56);
-    cout <<V.front()<<endl;
-    cout<<V.back()<<endl;
-    cout<<V.at(2)<<endl;
+    vector<int> M;
 
-    V.erase(V.begin()+2);
+    skaitomPusinam(M);
+    spausdink(M, "Nuskaite");
+    rikiavimas(M);
+    spausdink(M, "surikiuotas");
 
-    for(int i=0; i<V.size(); i++){
-         cout <<V[i]<<"  ";
-    }
 
-    if(V.empty()){
-        cout<<"Tuscias masyvas \n";
-    } else {cout<<"Pilnas  \n";}
+
+    in.close();
+    out.close();
     return 0;
+}
+void skaitomPusinam(vector<int> &A){
+    int a=0;
+    while(!in.eof()){
+        in>>a;
+        A.push_back(a);
+    }
+}
+void spausdink(vector<int> A, string text){
+    out<<text<<endl;
+    for(int i=0; i<A.size(); i++){
+        out<<A[i]<<" ";
+}
+out<<endl;
+}
+void rikiavimas(vector<int> &A){
+    int did, maz;
+    did = *max_element(A.begin(),A.end());
+    maz = *min_element(A.begin(),A.end());
+    cout<<did<<endl;
+    cout<<maz;
+    sort(A.begin(),A.end());
 }
